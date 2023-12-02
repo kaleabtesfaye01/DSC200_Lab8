18197be8b9f7bacfd2cec603af05be5d070354f0
Manogya Aryal <manogyaaryal39@gmail.com>
2023-12-02 15:46:06 -0500

    Pushing the 2nd part

--- Lab8_group2.py
+++ Lab8_group2.py
@@ -1,3 +1,17 @@
+"""
+Group 2 Lab8
+Kaleab Alemu
+Manogya Aryal
+
+This program takes data from 2 API's and returns the result in a csv file. One of the API requires authentication
+(The cat API) to access the data while the other (The dog API) does not.
+
+It has three functions: catAPI, dogAPI and the main. The catAPI is used to access the cat API and return the data in
+a csv file. The dogAPI is used to do the same as catAPI but getting the result from dog API. Finally, the main function
+is used to take inout from user about which API they would like to get the dataset from. It calls the respective
+function based on user's input.
+"""
+
 # Importing necessary libraries
 import requests
 import pandas as pd
@@ -5,6 +19,7 @@ import pandas as pd
 
 # Defining a function called catAPI
 def catAPI():
+
     """
     Retrieves cat images using TheCatAPI and stores the data in a CSV file.
 
@@ -38,7 +53,51 @@ def catAPI():
 
     # Saving the DataFrame to a CSV file named 'cat_group2.csv'
     df.to_csv('cat_group2.csv')
+"""
+This function extracts data (which is dog images) from Dog API we found in a public API repository in git, and outputs
+the result in  a csv file. This API does not require authentication. 
+"""
+def dogAPI():
+    api_url = 'https://dog.ceo/api/breeds/image/random/10' # this is the url for the api we are accessing.Here
+    # The "/api/breeds/image/random/10" endpoint is used, and the 10 specifies the limit of dog images to retrieve.
+
+    # Sending a GET request to the Dog API using the constructed URL
+    response = requests.get(api_url)
+
+    # Checking if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Creating a Pandas DataFrame from the JSON response received from the API
+        data = response.json()
+        df = pd.DataFrame(data['message'], columns=['url'])
+
+        # Adding an 'id' column to the DataFrame
+        df['id'] = df.index + 1
+
+        # Setting the 'id' column as the index of the DataFrame
+        df.set_index('id', inplace=True)
+
+        # Saving the DataFrame to a CSV file named 'dog_group2.csv'
+        df.to_csv('dog_group2.csv')
+    else: # printing an error message if the url is not accessible
+        print(f"Error: Unable to fetch data. Status code: {response.status_code}")
+
+# this is the main function that prompts user for input and returns the dataset of user's choice.
+def main():
+    user_choice = input("Enter what dataset you would like to access \n 1. Cat \n 2. Dogs \n: ")
+    if user_choice == "1":# Calling the catAPI function to execute the code if the user chooses to get dataset from
+        # CAT API
+        catAPI()
+    elif user_choice == "2": # Calling the dogAPI function to execute the code if the user chooses to get dataset from
+        # dog API
+        dogAPI()
+    else: # printing an error message if a user types invalid option and calling the main function again.
+        print("Invalid answer")
+        main()
+# Call the main function
+main()
 
 
-# Calling the catAPI function to execute the code
-catAPI()
+
+
+
+
